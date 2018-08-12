12f7d0689d21de85a754bcf06e94c3109fe3eded
Felipe Acquati <unomais@yahoo.com.br>
2018-08-12 01:40:00 +0000

    Added: Comment Form - with mat-slider.

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
@@ -6,6 +6,9 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { switchMap } from 'rxjs/operators';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -20,10 +23,41 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
+  
+  @ViewChild('fform') commentFormDirective;
+  
+  commentForm: FormGroup;
+  comment: Comment;
+  
+  formErrors = {
+    'author': '',
+    'rating': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.',
+      'maxlength':     'Name cannot be more than 25 characters long.'
+    },
+    'rating': {
+      'required':      'Tel. number is required.'
+    },
+    'comment': {
+      'required':      'Comment is required.',
+      'minlength':     'Comment must be at least 2 characters long.',
+      'maxlength':     'Comment cannot be more than 25 characters long.'
+    }
+  };
+
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
     // const id = +this.route.snapshot.params['id'];
@@ -45,6 +79,30 @@ export class DishdetailComponent implements OnInit {
   
   goBack(): void {
     this.location.back();
+  }
+  
+  createForm(): void {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      rating: ['5', ],
+      comment: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ]
+    });
+    
+    // this.commentForm.valueChanges
+    //   .subscribe(data => this.onValueChanged(data));
+
+    // this.onValueChanged(); // (re)set validation messages now
+  }
+  
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    console.log(this.comment);
+    this.commentForm.reset({
+      author: '',
+      rating: '',
+      comment: ''
+    });
+    this.commentFormDirective.resetForm();
   }
 
 }

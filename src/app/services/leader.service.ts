715ec4c1a6c97edbdcda650a8a864ccea51ee765
Felipe Acquati <felipe_acq@yahoo.com.br>
2018-08-19 18:48:28 -0300

    Updated: promotion and leader service using restangular.

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -3,55 +3,73 @@ import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map, catchError } from 'rxjs/operators';
+
+import { baseURL } from '../shared/baseurl';
+import { Restangular } from 'ngx-restangular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { 
-    
+  constructor(private restangular: Restangular) {
+
   }
-  
+
   /*
   getLeaders(): Promise<Leader[]> {
     // return new Promise(resolve => {
     //   // Simulate server latency with 2 second delay
     //   setTimeout( () => resolve(LEADERS), 2000);
     // });
-    
+
     return of(LEADERS).pipe(delay(2000)).toPromise();
   }
-  
+
   getLeader(id: number): Promise<Leader> {
     // return new Promise(resolve => {
     //   // Simulate server latency with 2 second delay
     //   setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
     // });
-    
+
     return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000)).toPromise();
   }
-  
+
   getFeaturedLeader(): Promise<Leader> {
     // return new Promise(resolve => {
     //   // Simulate server latency with 2 second delay
     //   setTimeout(() => resolve(LEADERS.filter((leader) => leader.featured)[0]), 2000);
     // });
-    
+
     return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000)).toPromise();
   }
   */
-  
+
+  // source data - leaders
+  // getLeaders(): Observable<Leader[]> {
+  //   return of(LEADERS).pipe(delay(2000));
+  // }
+  //
+  // getLeader(id: number): Observable<Leader> {
+  //   return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+  // }
+  //
+  // getFeaturedLeader(): Observable<Leader> {
+  //   return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+  // }
+
+  // source data - restangular
   getLeaders(): Observable<Leader[]> {
-    return of(LEADERS).pipe(delay(2000));
+    return this.restangular.all('leaders').getList();
   }
 
   getLeader(id: number): Observable<Leader> {
-    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+    return  this.restangular.one('leaders', id).get();
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+    return this.restangular.all('leaders').getList({featured: true})
+      .pipe(map(leaders => leaders[0]));
   }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -3,24 +3,27 @@ import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map, catchError } from 'rxjs/operators';
+
+import { baseURL } from '../shared/baseurl';
+import { Restangular } from 'ngx-restangular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() {
-    
+  constructor(private restangular: Restangular) {
+
   }
-  
+
   /*
   getPromotions(): Promise<Promotion[]> {
     // return new Promise(resolve => {
     //   // Simulate server latency with 2 second delay
     //   setTimeout(() => resolve(PROMOTIONS), 2000);
     // });
-    
+
     return of(PROMOTIONS).pipe(delay(2000)).toPromise();
   }
 
@@ -29,7 +32,7 @@ export class PromotionService {
     //   // Simulate server latency with 2 second delay
     //   setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
     // });
-    
+
     return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000)).toPromise();
   }
 
@@ -38,19 +41,35 @@ export class PromotionService {
     //   // Simulate server latency with 2 second delay
     //   setTimeout(() => resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]), 2000);
     // });
-    
+
     return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000)).toPromise();
   }*/
-  
+
+  // source data - promotions
+  // directly operate with observables
+  // getPromotions(): Observable<Promotion[]> {
+  //   return of(PROMOTIONS).pipe(delay(2000));
+  // }
+  //
+  // getPromotion(id: number): Observable<Promotion> {
+  //   return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+  // }
+  //
+  // getFeaturedPromotion(): Observable<Promotion> {
+  //   return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+  // }
+
+  // source data - restangular
   getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));
+    return this.restangular.all('promotions').getList();
   }
 
   getPromotion(id: number): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+    return  this.restangular.one('promotions', id).get();
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+    return this.restangular.all('promotions').getList({featured: true})
+      .pipe(map(promotions => promotions[0]));
   }
 }
